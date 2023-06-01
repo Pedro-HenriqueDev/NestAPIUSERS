dd0c8493d564f149f983b18228f7ddeb4a1466dd
Pedro-HenriqueDev <ph1305803@gmail.com>
2023-06-01 09:24:51 -0300

    create endpoint login and local strategy

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    
+    login() {
+        return "OPA"
+    }
+
+    validateUser(email: string, password: string) {
+        throw new Error('Method not implemented.');
+    }
+}
